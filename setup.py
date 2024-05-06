6724f9fdbf074bfcb1aee1aa7fd46528eb267f32
Gustavo Brunetto <gustavo.brunetto@earthdaily.com>
2024-05-06 13:48:04 -0700

    add dependencies version

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="mypackage",
+    name="ades_v2",
     version="0.1",
     packages=find_packages(),
     install_requires=[
-        "os",
-        "pathlib",
-        "json",
-        "loguru",
-        "requests",
-        "yaml",
+        "boto3~=1.34.99",
+        "requests~=2.31.0",
+        "PyYAML~=6.0.1",
+        "loguru~=0.7.2",
+        "pystac~=1.10.1",
+        "kubernetes~=29.0.0"
     ],
 )
