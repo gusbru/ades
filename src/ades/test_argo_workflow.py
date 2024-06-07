65a4c963b05a6db395717a96ab7d96728ced4d49
Gustavo Brunetto <gustavo.brunetto@earthdaily.com>
2024-06-07 11:19:39 -0700

    start to test run jobs o a single namespace

---
+++ src/ades/test_argo_workflow.py
@@ -0,0 +1,39 @@
+import json
+from typing import Any
+
+from loguru import logger
+import yaml
+
+
+from .argo_workflow import (
+    ArgoWorkflow,
+    JobInformation,
+    WorkflowConfig,
+)
+
+def main():
+    # load conf from file
+    with open('conf.json', 'r') as f:
+        conf = json.load(f)
+
+    # load inputs from file
+    with open('inputs.json', 'r') as f:
+        inputs = json.load(f)
+
+    with open("argo_template.yaml", "r") as stream: 
+        argo_template = yaml.safe_load(stream)
+
+    job_information = JobInformation(conf=conf, inputs=inputs)
+    
+    workflow_config = WorkflowConfig(
+        conf=conf,
+        job_information=job_information,
+    )
+
+    # run the workflow
+    logger.info("Running workflow")
+    argo_workflow = ArgoWorkflow(workflow_config=workflow_config)
+    exit_status = argo_workflow.run(workflow_file=argo_template)
+
+if __name__ == '__main__':
+    main()
